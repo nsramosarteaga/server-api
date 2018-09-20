0ff278f0430224f290761060d77142772fdda209
Nestor Ramos Arteaga <nsramosarteaga@gmail.com>
2018-09-19 22:51:06 -0500

    Update secrets.js

--- config/secrets.js
+++ config/secrets.js
@@ -1,7 +1,7 @@
 module.exports = {
   cloudinary: {
-    api_key: '164554511522492',
-    cloud_name: 'nestorsramosarteaga',
-    api_secret: 'b2sBm9i51ElgF1dAgPSizmd-6K8'
+    api_key: '######',
+    cloud_name: '#####',
+    api_secret: '#####'
   }
 }
