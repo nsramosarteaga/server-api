8258a35b360211de0a1308932a7e2b5012e5dfbf
Nestor Ramos Arteaga <nsramosarteaga@gmail.com>
2018-10-07 19:52:12 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbName = 'places_api';
 
 module.exports = {
-  connect: ()=> mongoose.connect('mongodb://localhost/'+dbName,  { useNewUrlParser: true }).
+  connect: ()=> mongoose.connect('mongodb://localhost/'+dbName,  { useNewUrlParser: true }),
   dbName,
   connection: ()=>{
     if(mongoose.connection)
